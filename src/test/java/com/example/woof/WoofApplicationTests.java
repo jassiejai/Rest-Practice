13a89646d6a07cf88338426bf138f567bfbcadfb
Jassie <jasminehart94@gmail.com>
2022-03-16 09:31:54 -0700

    created Java file

---
+++ src/test/java/com/example/woof/WoofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoofApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
